2ce7ed50e440ea55ac76d3cfc64983629fd9c308
Enrico Weigelt, metux IT service <weigelt@metux.de>
2009-03-07 18:02:36 +0100

    rostring: renamed funcs to mixpsrv_*(), added close and str is copied now

--- include/9p-mixpsrv/rostring_ops.h
+++ include/9p-mixpsrv/rostring_ops.h
@@ -6,16 +6,18 @@
 #include <9p-mixpsrv/types.h>
 
 /* -- file access to an in-memory string (read-only) -- */
-int         mixp_rostring_ops_init(MIXPSRV_FILE_HANDLE* f, const char* filename);
-int         mixp_rostring_ops_open(MIXPSRV_FILE_HANDLE* f, long mode);
-long        mixp_rostring_ops_read(MIXPSRV_FILE_HANDLE* f, long offset, size_t size, void* buf);
-long        mixp_rostring_ops_size(MIXPSRV_FILE_HANDLE* f);
-MIXPSRV_FILE_HANDLE* mixp_rostring_ops_create(const char* str);
+int         mixpsrv_rostring_ops_init(MIXPSRV_FILE_HANDLE* f, const char* filename);
+int         mixpsrv_rostring_ops_open(MIXPSRV_FILE_HANDLE* f, long mode);
+long        mixpsrv_rostring_ops_read(MIXPSRV_FILE_HANDLE* f, long offset, size_t size, void* buf);
+long        mixpsrv_rostring_ops_size(MIXPSRV_FILE_HANDLE* f);
+int         mixpsrv_rostring_ops_close(MIXPSRV_FILE_HANDLE* f);
+MIXPSRV_FILE_HANDLE* mixpsrv_rostring_ops_create(const char* str);
 
-static MIXPSRV_FILE_OPS ixp_rostring_ops = {
-    .open 	= mixp_rostring_ops_open,
-    .read 	= mixp_rostring_ops_read,
-    .size 	= mixp_rostring_ops_size,
+static MIXPSRV_FILE_OPS mixpsrv_rostring_ops = {
+    .open 	= mixpsrv_rostring_ops_open,
+    .read 	= mixpsrv_rostring_ops_read,
+    .size 	= mixpsrv_rostring_ops_size,
+    .close      = mixpsrv_rostring_ops_close,
     .classname 	= "rostring"
 };
 
--- libmixpsrv/rostring_ops.c
+++ libmixpsrv/rostring_ops.c
@@ -3,19 +3,23 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include <9p-mixpsrv/types.h>
 #include <9p-mixpsrv/p9srv.h>
 #include <9p-mixpsrv/rostring_ops.h>
 
-int mixp_rostring_ops_open(MIXPSRV_FILE_HANDLE* f, long mode)
+int mixpsrv_rostring_ops_open(MIXPSRV_FILE_HANDLE* f, long mode)
 {
     // FIXME: we currently are only working RO on plain files.
     return 0;
 }
 
-long mixp_rostring_ops_read(MIXPSRV_FILE_HANDLE* f, long offset, size_t size, void* buf)
+long mixpsrv_rostring_ops_read(MIXPSRV_FILE_HANDLE* f, long offset, size_t size, void* buf)
 {
+    if (!f->priv.name)
+	return 0;
+
     if (offset>=(f->priv.fd))
     {
 	fprintf(stderr,"ixp_rostring_ops_read() attempt to ready beyond end of file\n");
@@ -26,36 +30,44 @@ long mixp_rostring_ops_read(MIXPSRV_FILE_HANDLE* f, long offset, size_t size, vo
 	size = (f->priv.fd) - offset;
 	fprintf(stderr,"ixp_rostring_ops_read() reducing size to %uld @ offset %ld size %d\n", size, offset, f->priv.fd);
     }
-    
+
     memcpy(buf,(f->priv.name)+offset, size);
     return size;
 }
 
-MIXPSRV_FILE_HANDLE* mixp_rostring_ops_create(const char* str)
+MIXPSRV_FILE_HANDLE* mixpsrv_rostring_ops_create(const char* str)
 {
     MIXPSRV_FILE_HANDLE* f = p9srv_get_file();
-    mixp_rostring_ops_init(f,str);
+    mixpsrv_rostring_ops_init(f,str);
     return f;
 }
 
-int mixp_rostring_ops_init(MIXPSRV_FILE_HANDLE* f, const char* str)
+int mixpsrv_rostring_ops_init(MIXPSRV_FILE_HANDLE* f, const char* str)
 {
-    if (str==NULL)
-	f->priv.name = "";
-    else
-	f->priv.name = str;
-    f->priv.fd   = strlen(f->priv.name);
-    f->ops = ixp_rostring_ops;
+    f->priv.name = (str ? strdup(str) : strdup(""));
+    f->priv.fd  = strlen(f->priv.name);
+    f->ops = mixpsrv_rostring_ops;
     return 0;
 }
 
-long mixp_rostring_ops_size(MIXPSRV_FILE_HANDLE* f)
+long mixpsrv_rostring_ops_size(MIXPSRV_FILE_HANDLE* f)
 {
     return f->priv.fd;
 }
 
-int mixp_rostring_ops_stat(MIXPSRV_FILE_HANDLE* f, MIXP_STAT* s)
+int mixpsrv_rostring_ops_stat(MIXPSRV_FILE_HANDLE* f, MIXP_STAT* s)
 {
     fprintf(stderr,"ixp_rostring_ops_size() stat not implemented yet\n");
+    return 0;
+}
+
+int mixpsrv_rostring_ops_close(MIXPSRV_FILE_HANDLE* f)
+{
+    if (f->priv.name)
+    {
+	free((char*)f->priv.name);
+	f->priv.name = NULL;
+	f->priv.fd   = 0;
+    }
     return 0;
 }

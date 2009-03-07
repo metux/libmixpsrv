9c056f3fa5c28a3e78b2f4184a50f63314fde1c3
Enrico Weigelt, metux IT service <weigelt@metux.de>
2009-03-07 14:31:43 +0100

    include/9p-mixpsrv/default_ops.h: Added some macros for simpler lookup ops

--- include/9p-mixpsrv/default_ops.h
+++ include/9p-mixpsrv/default_ops.h
@@ -1,6 +1,6 @@
 
-#ifndef __IXPSERV_DEFAULT_OPS_H
-#define __IXPSERV_DEFAULT_OPS_H
+#ifndef __MIXPSRV_DEFAULT_OPS_H
+#define __MIXPSRV_DEFAULT_OPS_H
 
 #include <9p-mixp/mixp.h>
 #include <9p-mixpsrv/types.h>
@@ -14,5 +14,28 @@ int  mixpsrv_default_ops_stat(MIXPSRV_FILE_HANDLE* f, MIXP_STAT* s);
 int  mixpsrv_default_ops_close(MIXPSRV_FILE_HANDLE* f);
 
 extern MIXPSRV_FILE_OPS mixp_default_fileops;
+
+/* some helper macros for the lookup() ops */
+
+#define MIXPSRV_LOOKUP_BEGIN						\
+    MIXPSRV_FILE_HANDLE* ___lookup_first = NULL;			\
+    MIXPSRV_FILE_HANDLE* ___lookup_walk  = NULL;			\
+
+#define MIXPSRV_LOOKUP_ADD(par)					\
+    {									\
+	MIXPSRV_FILE_HANDLE* ___lookup_tmp = par;			\
+	if (par)							\
+	{								\
+	    if (___lookup_first == NULL)				\
+		___lookup_first = ___lookup_walk = ___lookup_tmp;	\
+	    else							\
+	    {								\
+		___lookup_walk->next = ___lookup_tmp;			\
+		___lookup_walk = ___lookup_tmp;				\
+	    }								\
+	}								\
+    }
+
+#define MIXPSRV_LOOKUP_END return ___lookup_first;
 
 #endif
